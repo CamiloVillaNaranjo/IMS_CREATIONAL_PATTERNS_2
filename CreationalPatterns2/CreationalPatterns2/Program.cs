3aa2d55909cbb41d1421c73c7ae39bfe1672f16d
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-03-09 03:29:03 -0500

    Adding first part

---
+++ CreationalPatterns2/CreationalPatterns2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
