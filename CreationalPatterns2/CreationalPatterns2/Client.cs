a9310dc6f014e4ef4dec0bb60f1693738bbd2bc1
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-03-09 03:50:31 -0500

    ObjectPool example

---
+++ CreationalPatterns2/CreationalPatterns2/Client.cs
@@ -0,0 +1,36 @@
+﻿namespace CreationalPatterns2
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            AutomatedPicker pickerA = PickerPool.GetPicker();
+            AutomatedPicker pickerB = PickerPool.GetPicker();
+
+            pickerA.Identify("PA");
+            pickerB.Identify("PB");
+
+            pickerA.GoToLocation("Bay 1");
+            pickerB.GoToLocation("Bay 3");
+
+            pickerA.Pick("Processor");
+            pickerB.Pick("RAM");
+
+            pickerA.GoToLocation("Build Room");
+            pickerB.GoToLocation("Build Room");
+
+            pickerA.Drop();
+            pickerB.Drop();
+
+            PickerPool.ReleasePicker(pickerA);
+            PickerPool.ReleasePicker(pickerB);
+
+            AutomatedPicker pickerC = PickerPool.GetPicker();
+            AutomatedPicker pickerD = PickerPool.GetPicker();
+            AutomatedPicker pickerE = PickerPool.GetPicker();
+            AutomatedPicker pickerF = PickerPool.GetPicker();
+            AutomatedPicker pickerG = PickerPool.GetPicker();
+            AutomatedPicker pickerH = PickerPool.GetPicker(); //Crashes Error thrown here.
+        }
+    }
+}
---
+++ CreationalPatterns2/CreationalPatterns2/PickerPool.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CreationalPatterns2
+{
+    public class PickerPool
+    {
+        const int PickerCount = 5;
+
+        private static List<AutomatedPicker> _available = new List<AutomatedPicker>();
+        private static List<AutomatedPicker> _inUse = new List<AutomatedPicker>();
+
+        static PickerPool()
+        {
+            for (int i = 1; i <= PickerCount; i++)
+            {
+                _available.Add(new AutomatedPicker(i));
+            }
+        }
+
+        public static AutomatedPicker GetPicker()
+        {
+            lock (_available)
+            {
+                if (_available.Count != 0)
+                {
+                    AutomatedPicker picker = _available[0];
+                    _inUse.Add(picker);
+                    _available.RemoveAt(0);
+                    return picker;
+                }
+                else
+                {
+                    throw new InvalidOperationException("No pickers are available");
+                }
+            }
+        }
+
+        public static void ReleasePicker(AutomatedPicker picker)
+        {
+            Reset(picker);
+
+            lock (_available)
+            {
+                _available.Add(picker);
+                _inUse.Remove(picker);
+            }
+        }
+
+        private static void Reset(AutomatedPicker picker)
+        {
+            if (picker.Carrying != null)
+            {
+                picker.Drop();
+            }
+
+            picker.GoToLocation("Recharging Station");
+        }
+    }
+}
